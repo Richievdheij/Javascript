2d147cf87e0751a30c22f28a2548b4004c36d260
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2022-04-04 11:06:22 +0200

    B1W2L3.JS
    1. Maak een JavaScript applicatie die de tafel van 6 uitrekent.
    
    2. Maak een JavaScript applicatie die de tafel van 3 uitrekent
    
    Vraag: hoe kun je er op een handige manier voor zorgen dat je gemakkelijk een ander tafel kunt uitrekenen, zonder dat je al te veel wijzigingen in de code moet doen?
    
    3. Berekeningen uitvoeren
    
    Zorg met tekst er ook voor dat je de berekening kunt zien (en niet alleen de uitkomst):
    
    Gebruik het getal 12 als basis voor de volgende berekeningen:
    Tel bij het getal 6 op
    Vermenigvuldig vervolgens de uitkomst met 10
    Deel de uitkomst door 5
    Haal van de uitkomst 12 af
    
    4. Berekeningen uitvoeren met invoer van getal
    
    Laat de berekeningen bij punt 3 staan in de JavaScript file en voer dezelfde berekeningen uit zoals beschreven staan bij stap 3, echter nu vraag je de gebruiker om een getal in te voeren. Voer deze berekeningen eerst uit met het getal 12 en daarna vul je een paar andere getallen in.

--- B1W2L3/b1w2l3.js
+++ B1W2L3/b1w2l3.js
@@ -1,3 +1,20 @@
 // begin hier je JavaScript commandos
-// Calculate the table of 3 and 6
+// 1. Maak een JavaScript applicatie die de tafel van 3 en 6 uitrekent.
+
+// Program to generate a multiplication table
+
+// Take input from the user
+const number = parseInt(prompt('Enter an integer: '));
+
+// Creating a multiplication table
+for(let i = 1; i <= 10; i++) {
+
+    // Multiply i with number
+    const result = i * number;
+
+    // Display the result
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+
 
